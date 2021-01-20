b9a98e03727907346c7d74e4095861e3b026c020
chaitrarai00 <chaitrarai00@gmail.com>
2021-01-20 16:12:44 +0530

    Annotation-Based Dependency Injection in Spring

--- src/main/java/com/dependencyinjection/configuration/DIConfiguartion.java
+++ src/main/java/com/dependencyinjection/configuration/DIConfiguartion.java
@@ -1,5 +1,19 @@
 package com.dependencyinjection.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.dependencyinjection.services.EmailMessageService;
+import com.dependencyinjection.services.MessageService;
+
+@Configuration
+@ComponentScan(value="com.dependencyinjection.consumer")
 public class DIConfiguartion {
+	
+	@Bean
+	public MessageService getMessageService() {
+		return new EmailMessageService();
+	}
 
 }
--- src/main/java/com/example/demo/SpringCoreApplication.java
+++ src/main/java/com/example/demo/SpringCoreApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
+import com.dependencyinjection.configuration.DIConfiguartion;
+import com.dependencyinjection.consumer.MyAnnotationApplication;
+
+//@SpringBootApplication
 public class SpringCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCoreApplication.class, args);
+		//SpringApplication.run(SpringCoreApplication.class, args);
+		AnnotationConfigApplicationContext annotationConfigApplicationContext
+		=new AnnotationConfigApplicationContext(DIConfiguartion.class);
+		MyAnnotationApplication myAnnotationApplication=
+				annotationConfigApplicationContext.getBean(MyAnnotationApplication.class);
+		myAnnotationApplication.processMessage
+		("You got to code and not listen to anyone", "Youfromthefuture@wellwish.com");
+		annotationConfigApplicationContext.close();
 	}
 
 }
